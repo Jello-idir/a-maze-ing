d242e248b80b74eb6f036a0a6d620252380f4338
Jello <127986992+Jello-idir@users.noreply.github.com>
2026-04-01 12:36:56 +0000

    feat: color paramters in maze

--- asciify/display.py
+++ asciify/display.py
@@ -1,156 +1,127 @@
-#Each cell in the maze is written as a single hexadecimal digit,
+from enum import Enum
+from typing import TextIO
+import io
+
+
+
+class Color(Enum):
+    OPEN = "\033[48;2;30;35;50m"
+    WALL = "\033[48;2;255;255;255m"
+    CELL = "\033[48;2;30;50;80m"
+    RESET = "\033[0m"
+    BLOCK = "\033[48;2;0;100;200m"
+
+
+class Block(Enum):
+    OPEN = f"{Color.OPEN.value}  {Color.RESET.value}"
+    WALL = f"{Color.WALL.value}  {Color.RESET.value}"
+    CELL = f"{Color.CELL.value}    {Color.RESET.value}"
+    BLOCK = f"{Color.BLOCK.value}    {Color.RESET.value}"
+
+
+
+class AsciiCell:
+    def __init__(self, hex_value: str):
+        self.hex_value = hex_value
+        self.value = int(hex_value, 16)
+        self.north, self.east, self.south, self.west = [
+            (self.value >> 0) & 1,
+            (self.value >> 1) & 1,
+            (self.value >> 2) & 1,
+            (self.value >> 3) & 1,
+        ]
+        self.is_last: bool = False
+
+    def binary_repr(self) -> str:
+        return f"{self.west}{self.south}{self.east}{self.north}"
+
+    @staticmethod
+    def hex_to_bool(hex: int) -> list[bool]:
+        pass
+
+
+def parse_cells(cells_str: str) -> list[list[AsciiCell]]:
+    cells: list[list[AsciiCell]] = []
+
+    lines = cells_str.splitlines()
+    for line in lines:
+        cell_row: list[AsciiCell] = []
+        for hex_digit in line.strip():
+            cell_row.append(AsciiCell(hex_digit))
+        cells.append(cell_row)
+        cell_row[-1].is_last = True
+    return cells
+
+
+class Maze:
+    def __init__(self, maze: str | TextIO):
+        if isinstance(maze, io.IOBase):
+            maze = maze.read()
+
+        self.maze = parse_cells(maze)
+        self.width = len(self.maze[0])
+        self.height = len(self.maze)
+
+    def display_maze(self) -> None:
+
+        # upper line
+        for cell in self.maze[0]:
+            print(Block.WALL.value, end="")
+            if (cell.north):
+                print(Block.WALL.value * 2, end="")
+            else:
+                print(Block.OPEN.value * 2, end="")
+        print(Block.WALL.value)
+
+        # print lines
+        for row in self.maze:
+
+            # middlline
+            for _ in range(2):
+                if row[0].west:
+                    print(Block.WALL.value, end="")
+                else:
+                    print(Block.OPEN.value, end="")
+
+                for cell in row:
+                    if cell.hex_value == "F":
+                        print(Block.BLOCK.value, end="")
+                    else:
+                        print(Block.CELL.value, end="")
+                    if cell.east:
+                        print(Block.WALL.value, end="")
+                    else:
+                        print(Block.OPEN.value, end="")
+                print()
+
+            # bottom line
+            print(Block.WALL.value, end="")
+            for cell in row:
+                if cell.south:
+                    print(Block.WALL.value * 2, end="")
+                else:
+                    print(Block.OPEN.value * 2, end="")
+                print(Block.WALL.value, end="")
+            print()
+
+
+def test():
+    with open("maze_example.txt", "r") as fl:
+        maze = Maze(fl)
+    print("\n" * 120)
+    maze.display_maze()
+    print("\n" * 5)
+
+
+if __name__ == "__main__":
+    test()
+    
+    
+    
+# Each cell in the maze is written as a single hexadecimal digit,
 # where the digit encodes the state of that cell's four walls as a 4-bit value.
 
 # bit 0 = 1 → North wall closed
 # bit 1 = 1 → East wall closed
 # bit 2 = 0 → South wall open
 # bit 3 = 0 → West wall open
-
-from enum import Enum
-
-
-class Block(Enum):
-    OPEN = " "
-    WALL = "█"
-    CELL = "  "
-
-
-class Color(Enum):
-    BLACK = "\033[30m"
-    RED = "\033[31m"
-    GREEN = "\033[32m"
-    YELLOW = "\033[33m"
-    BLUE = "\033[34m"
-    MAGENTA = "\033[35m"
-    CYAN = "\033[36m"
-    WHITE = "\033[37m"
-    RESET = "\033[0m"
-
-
-class Cell:
-    def __init__(self, hex_value: str):
-        self.hex_value = hex_value
-        self.value = int(hex_value, 16)
-        self.north = (self.value >> 0) & 1
-        self.east = (self.value >> 1) & 1
-        self.south = (self.value >> 2) & 1
-        self.west = (self.value >> 3) & 1
-        self.is_last = False
-
-    def binary_repr(self) -> str:
-        return f"{self.west}{self.south}{self.east}{self.north}"
-
-    def closed_walls(self) -> list[str]:
-        walls = []
-        if self.north:
-            walls.append("N")
-        if self.east:
-            walls.append("E")
-        if self.south:
-            walls.append("S")
-        if self.west:
-            walls.append("W")
-        return walls
-
-    
-    def open_walls(self) -> list[str]:
-        walls = []
-        if not self.north:
-            walls.append("N")
-        if not self.east:
-            walls.append("E")
-        if not self.south:
-            walls.append("S")
-        if not self.west:
-            walls.append("W")
-        return walls
-
-
-class Row:
-    def __init__(self, cells: list[Cell]):
-        self.cells = cells
-
-    def print_row(self):
-        # first print the north and south walls
-        print(Block.WALL.value, end="")
-        for cell in self.cells:
-            print(Block.CELL.value, end="")
-            if cell.east or cell.is_last:
-                print(Block.WALL.value, end="")
-            else:
-                print(Block.OPEN.value, end="")
-        print()
-
-        # second print the south walls
-        print(Block.WALL.value, end="")
-        for cell in self.cells:
-            print(Block.CELL.value, end="")
-            if cell.east or cell.is_last: 
-                print(Block.WALL.value, end="")
-            else:
-                print(Block.OPEN.value, end="")
-        print()
-
-        # third print the south walls
-        print(Block.WALL.value, end="")
-        for cell in self.cells:
-            if cell.south:
-                print(Block.WALL.value * 3, end="")
-            elif cell.is_last:
-                print(Block.OPEN.value * 2, end="")
-                print(Block.WALL.value, end="")
-            else:
-                print(Block.OPEN.value * 2, end="")
-                print(Block.WALL.value, end="")
-        print()
-
-
-
-    
-    
-
-def parse_instructions(instructions: str) -> list[list[Cell]]:
-    cells: list[list[Cell]] = []
-
-    lines = instructions.strip().splitlines()
-
-    for line in lines:
-        cell_row: list[Cell] = []
-        for hex_digit in line.strip():
-            cell_row.append(Cell(hex_digit))
-        cells.append(cell_row)
-        cell_row[-1].is_last = True
-    return cells
-            
-
-def print_upper_wall(width: int):
-    for _ in range(width):
-        print(Block.WALL.value * 3, end="")
-    print(Block.WALL.value)
-
-# def ascii_cells(fd, instructions) -> None:
-
-
-def print_maze(instructions: str) -> None:
-
-    print()
-    width = len(instructions.splitlines()[0])
-    height = len(instructions.splitlines())
-    cells = parse_instructions(instructions)
-    print_upper_wall(width)
-    rows = [Row(cell_row) for cell_row in cells]
-    for row in rows:
-        row.print_row()
-
-    print()
-
-def test():
-    with open("maze_example.txt", "r") as f:
-        instructions = f.read()
-
-    print_maze(instructions)
-
-
-
-if __name__ == "__main__":
-    test()
---
+++ asciify/loop.sh
@@ -0,0 +1,6 @@
+while true; do
+	printf "%100s" " " | tr " " "\n";
+	python display.py;
+	sleep 1;
+
+done
